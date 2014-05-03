0edb579778fcb6948495a94444680b16f30f8eef
spezi77 <spezialitz@gmx.de>
2014-05-03 16:50:51 +0200

    Clean up

--- products/keyboard.mk
+++ products/keyboard.mk
@@ -1,7 +1,6 @@
-LOCAL_PATH:= vendor/liquid/prebuilt/common  
+LOCAL_PATH:= vendor/liquid/prebuilt/common
 
-PRODUCT_COPY_FILES +
-    #$(LOCAL_PATH)/apps/GoogleLatinIme.apk:system/app/GoogleLatinIme.apk \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lib/libjni_unbundled_latinimegoogle.so:system/lib/libjni_unbundled_latinimegoogle.so \
     $(LOCAL_PATH)/lib/libjni_latinime.so:system/lib/libjni_latinime.so
 
